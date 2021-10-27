db2b641ba6a3f5fdb9cc4334842d4967853446aa
skygundam001 <40291831+skygundam001@users.noreply.github.com>
2021-10-27 18:57:23 +0800

    Update VM.js

--- VM.js
+++ VM.js
@@ -8,7 +8,7 @@ var parameter = {
 $.get('https://script.google.com/macros/s/AKfycbzOuKwdIMjjG8aH4uk5h4vuFmfirsb6Iog--mRbmCV6FBRFSjqoJ6xaRHV__vidhhYp/exec', parameter, function(data) {
   console.log(data);
   $(document).ready(function(){
-    $("showMySheetsData").html(data);
+    $("#showMySheetsData").html(data);
   });
   
 });
